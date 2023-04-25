3c845754b11cfea1e324fa67c2f9fe72529c212c
Laksh <raftaarmalhotra@gmail.com>
2023-04-25 23:40:28 +0530

    guess_the_number

---
+++ madlibs/madlibs.py
@@ -0,0 +1,17 @@
+# string concatenation that is how to put strings together
+
+#youtuber = 'Kirito'
+
+#print("subscribe to " + youtuber)
+#print("subscribe to {}".format(youtuber))
+#print(f"subscribe to {youtuber}")
+
+adj = input("Adjective: ")
+verb = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+I love to {verb}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
